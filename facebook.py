81b42b567b404d70a6328ac16d9196b0c45da020
shayqjan122233 <110079939+shayqjan122233@users.noreply.github.com>
2022-08-26 14:57:49 +0430

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+from time import sleep
+
+
+print("lanxnxn")
+while True:
+	
+	webbrowser.open("https://vpnifyapp.com/blog/page/3/what-is-an-isp-and-how-can-you-protect-yourself-from-being-tracked/")
+	sleep(5)
